6930c1809f21ffdf94ee197a1a181e72fcc11483
maksimzmacinskij <maxzhmach@mail.ru>
2024-12-30 01:21:25 +0300

    [master] - add BaseTypes.ts

--- src/core/config/BaseTypes.ts
+++ src/core/config/BaseTypes.ts
@@ -7,8 +7,11 @@ export const BASETYPES = {
     ResourceLoader: Symbol.for('ResourceLoader'),
     UpdateHandler: Symbol.for('UpdateHandler'),
     Update: Symbol.for('Update'),
+    UpdatableItems: Symbol.for('UpdatableItems'),
     Init: Symbol.for('Init'),
+    InitItems: Symbol.for('InitItems'),
     Reset: Symbol.for('Reset'),
+    ResetItems: Symbol.for('ResetItems'),
     GUI: Symbol.for('GUI'),
     PassedTime: Symbol.for('PassedTime'),
 }
